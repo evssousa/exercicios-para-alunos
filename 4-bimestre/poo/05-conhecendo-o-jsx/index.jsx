6c7933b988d770d6234b65d8c9bde4bcae7aa6dd
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2024-10-25 07:47:48 -0300

    up

---
+++ 4-bimestre/poo/05-conhecendo-o-jsx/index.jsx
@@ -0,0 +1,15 @@
+// o JSX trás os recursos que trabalhamos no HTML, mas por si só ele não é reconhecido pelo navegador. Por isso ele é compilado e isso facilita o desenvolvimento.
+const element = <h1>Olá mundo!</h1>
+
+const anotherElement = (
+    <div>
+        <h1>Texto</h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+)
